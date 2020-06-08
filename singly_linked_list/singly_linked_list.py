b3b9443a056141052a4d75420bf0bae514771a0e
Logan Van Hook <loganvan@att.net>
2020-06-08 15:08:19 -0400

    Stack

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -16,6 +16,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def add_to_tail(self, value):
         new_node = Node(value)
@@ -26,9 +27,12 @@ class LinkedList:
         else:
             self.tail.set_next(new_node)
             self.tail = new_node
+        
+        self.length += 1
 
     def remove_head(self):
         if self.head:
+            self.length -= 1
             if (self.tail is self.head):
                 ref_value = self.head.value
                 self.tail = None
@@ -38,6 +42,30 @@ class LinkedList:
                 ref_value = self.head.value
                 self.head = self.head.next_node
                 return ref_value
+
+    def remove_tail(self):
+        if (self.tail is None):
+            return None
+
+        if (self.tail is self.head):
+                ref_value = self.head.value
+                self.tail = None
+                self.head = None
+                self.length -= 1
+                return ref_value
+
+        current = self.head
+        last = None
+
+        while True:
+            if self.tail is current:
+                val = self.tail.value
+                self.tail = last
+                self.length -= 1
+                return val
+            else:
+                last = current
+                current = current.next_node
 
     def contains(self, value):
         if not self.head:
@@ -79,3 +107,6 @@ class LinkedList:
             current = current.next_node
         
         return highestvalue
+
+    def __len__(self):
+        return self.length
--- stack/stack.py
+++ stack/stack.py
@@ -10,19 +10,40 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.storage.__len__()
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if (self.storage.__len__() < 1):
+#             return None
+
+#         return self.storage.pop()
+
+import sys
+sys.path.append('./singly_linked_list')
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.storage.__len__()
 
     def push(self, value):
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def pop(self):
         if (self.storage.__len__() < 1):
             return None
 
-        return self.storage.pop()
+        return self.storage.remove_tail()
